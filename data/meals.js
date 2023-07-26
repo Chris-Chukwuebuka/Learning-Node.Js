3dda3e70b004faaa9ee27a560d7456c93e660ebc
Ebuka-Chucks <Chrisbrandford4@gmail.com>
2023-07-26 15:33:37 +0100

    first commit

---
+++ data/meals.js
@@ -0,0 +1,31 @@
+const meals = [
+    {
+        id: 1,
+        name: "Fried Rice",
+    },
+     {
+        id: 2,
+        name: "Ofada Rice",
+    },
+      {
+        id: 3,
+        name: "Spiced Rice",
+    },
+       {
+        id: 4,
+        name: "Singapore Rice",
+    },
+        {
+        id: 5,
+        name: "Fried Bread",
+    },
+         {
+        id: 6,
+        name: "Smooked Rice",
+    },   {
+        id: 7,
+        name: "Jellof Rice",
+    },
+]
+
+module.exports = meals;
